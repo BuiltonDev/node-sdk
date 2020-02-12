332b1bb5bc7f15aeb7e565a114a54b6e4c7eb299
Tigran <tigrangit@gmail.com>
2020-02-12 15:02:19 +0200

    revert service account key

--- test/collection/image.js
+++ test/collection/image.js
@@ -6,7 +6,7 @@ const Builton = require('../../src/main.js');
 const imageFile = require('../fetchmock/image.json');
 
 const endpoint = 'https://example.com';
-const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+const bearerToken = 'SERVICE_ACCOUNT_KEY';
 const sa = new Builton({ apiKey: 'dummy', bearerToken, endpoint });
 
 describe('Image related tests', () => {
